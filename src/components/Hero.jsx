f565c25402dd83cbdca954b2ad5fac6eb1b9baff
An Nguyen <annguyen5334@gmail.com>
2023-07-11 15:00:49 -0400

    hero update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,13 @@ import React from "react";
 const Hero = () => {
   return (
     <div className="text-white">
-
+      <div className="h-screen mt-[-96px] w-full mx-auto flex flex-col justify-center items-center">
+      <p>GROWING WITH DATA ANALYTICS</p>
+      <h1>Grow with data</h1>
+      <div>
+        <p>Fast, flexible financing for</p>
+      </div>
+    </div>
     </div>
   )
 }
