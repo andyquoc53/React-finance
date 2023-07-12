b7a4ef8eda78bc2f83f55b815d1b03656fd907c8
An Nguyen <annguyen5334@gmail.com>
2023-07-11 20:32:24 -0400

    Second commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,10 +4,10 @@ const Hero = () => {
   return (
     <div className="text-white">
       <div className="h-screen mt-[-96px] w-full mx-auto flex flex-col justify-center items-center">
-      <p>GROWING WITH DATA ANALYTICS</p>
-      <h1>Grow with data</h1>
+      <p className="text-[#00df9a] sm:text-3xl text-2xl">GROWING WITH DATA ANALYTICS</p>
+      <h1 className="font-bold sm:text-5xl text-4xl p-4">Grow with data.</h1>
       <div>
-        <p>Fast, flexible financing for</p>
+        <p className="text-2xl sm:text-xl pt-3">Fast, flexible financing for</p>
       </div>
     </div>
     </div>
